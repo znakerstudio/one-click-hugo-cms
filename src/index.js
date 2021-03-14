0c2aea1813a4bdf5670ed3cdcbd67b3846c39747
Znaker Studio <79854174+znakerstudio@users.noreply.github.com>
2021-03-14 20:42:40 +0100

    sc

--- src/index.js
+++ src/index.js
@@ -4,19 +4,3 @@ import "./css/main.css";
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
-
-<script>
-    if('serviceWorker' in navigator) {
-        window.addEventListiner('load', () => {
-            navigator.serviceWorker.register('sw.js')
-                .then((registration) => {
-                    console.log(`service worker registered succesfully ${registration}`)
-                })
-                .catch((err) => {
-                    console.log(`Error registring ${err}`)
-                })
-        })
-    } else {
-        console.log(`Service worker is not supported in this browser.`)
-    }
-</script>
