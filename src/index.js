6eb5652fa4b02eee9a1c83c534e5c7bc413d2ed1
Znaker Studio <79854174+znakerstudio@users.noreply.github.com>
2021-03-14 20:30:06 +0100

    many

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 // JS Goes here - ES6 supported
-
+if('serviceWorker' in navigator) {
+        window.addEventListiner('load', () => {
+            navigator.serviceWorker.register('sw.js')
+                .then((registration) => {
+                    console.log(`service worker registered succesfully ${registration}`)
+                })
+                .catch((err) => {
+                    console.log(`Error registring ${err}`)
+                })
+        })
+    } else {
+        console.log(`Service worker is not supported in this browser.`)
+    }
 import "./css/main.css";
 
 // Say hello
