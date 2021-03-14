caed934360239dc6c56903f2a37259b0a15d9549
Znaker Studio <79854174+znakerstudio@users.noreply.github.com>
2021-03-14 20:41:47 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import "./css/main.css";
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
 
-
 <script>
     if('serviceWorker' in navigator) {
         window.addEventListiner('load', () => {
