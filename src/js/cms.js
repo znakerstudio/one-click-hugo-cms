c237cd802155b8445af379c9e9ed85b88b9be603
Znaker Studio <79854174+znakerstudio@users.noreply.github.com>
2021-03-16 21:38:22 +0100

    przyklady
    many

--- src/js/cms.js
+++ src/js/cms.js
@@ -7,7 +7,7 @@ import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../c
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
 import ProductsPreview from "./cms-preview-templates/oferta";
-import ValuesPreview from "./cms-preview-templates/values";
+import ValuesPreview from "./cms-preview-templates/przyklady";
 import ContactPreview from "./cms-preview-templates/contact";
 
 CMS.registerPreviewStyle(styles, { raw: true });
